1dcabb59a366b49bdef95ce7a6c0029ab81b6760
Fien Lute <fienlute@MacBook-Air-van-Fien.local>
2016-11-20 22:37:48 +0100

    Initial Commit

---
+++ fienlute-pset4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fienlute-pset4
+//
+//  Created by Fien Lute on 20-11-16.
+//  Copyright © 2016 Fien Lute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
